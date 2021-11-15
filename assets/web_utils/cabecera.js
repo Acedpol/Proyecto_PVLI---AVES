45e73d26c69bff09738df38ee84d101f1a1067ed
Pablo Cubells <pablocub@ucm.es>
2021-11-15 23:22:24 +0100

    Dowload section - PACKAGE

--- assets/web_utils/cabecera.js
+++ assets/web_utils/cabecera.js
@@ -1,4 +1,14 @@
 document.write(`
+<div id="download-link" class="download">
+    <div style="height: 10%;"><h1 class="download-title" style="text-align: center;"> ¡Descarga con un click! </h1></div>
+    <div style="height: 10%;">
+        <button>
+            <a onclick="download_project()" href="https://github.com/Acedpol/Proyecto_PVLI---AVES/archive/refs/heads/master.zip">AVES.zip</a>
+        </button>
+    </div>
+</div>
+<noscript>Sorry, your browser does not support JavaScript!</noscript>
+
 <div class="container-fluid">
             <div class="row">
                 <div class="col-logo">
@@ -10,7 +20,7 @@ document.write(`
                         <ul>
                             <li><a id="menu-btn" href="index.html">HOME</a></li>                                                    
                             <li><a id="menu-btn" href="about.html">ABOUT</a></li>
-                            <li><a id="menu-btn" href="package.html">PACKAGE</a></li>
+                            <li><a id="package-btn" class="package-btn" onclick="toggle_download()">PACKAGE</a></li>
                             <li><a id="menu-btn" href="training.html">TRAINING</a></li>
                             <li><a id="menu-btn" href="contact-us.html">CONTACT US</a></li>
                         </ul>
@@ -20,7 +30,7 @@ document.write(`
                             <div class="overlay-content">
                                 <a id="overlay-btn" href="index.html">HOME</a>
                                 <a id="overlay-btn" href="about.html">ABOUT</a>
-                                <a id="overlay-btn" href="package.html">PACKAGE</a>
+                                <a id="overlay-btn" onclick="toggle_download()">PACKAGE</a>
                                 <a id="overlay-btn" href="training.html">TRAINING</a>
                                 <a id="overlay-btn" href="contact-us.html">CONTACT US</a>
                             </div>
--- assets/web_utils/overlay.js
+++ assets/web_utils/overlay.js
@@ -6,3 +6,33 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+
+// Toggle download window and start download:
+function toggle_download()
+{
+    var obj = document.getElementById("download-link");
+    var btn = document.getElementById("package-btn");
+    if(obj.style.display == "flex") {
+        obj.style.display = "none";
+        btn.style.backgroundColor = "unset";
+        btn.style.textDecorationLine = "unset";
+    }
+    else {
+        obj.style.display = "flex";
+        btn.style.backgroundColor = "rgba(0,0,0,0.92)";
+        btn.style.textDecorationLine = "underline";
+    }
+    console.log("Haz click para descargar");
+    
+    closeNav();
+}
+function download_project()
+{
+    var obj = document.getElementById("download-link");
+    obj.style.display = "none";
+    console.log("La descarga ha comenzado");
+
+    var btn = document.getElementById("package-btn");
+    btn.style.backgroundColor = "unset";
+    btn.style.textDecorationLine = "unset";
+}
