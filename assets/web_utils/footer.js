161db1d7e4916f1c1a3092ab0cfbe11d5489a126
Pablo Cubells <pablocub@ucm.es>
2021-11-16 17:28:04 +0100

    Links del footer funcionando

--- assets/web_utils/footer.js
+++ assets/web_utils/footer.js
@@ -2,13 +2,13 @@ document.write(`
 <div class="container-fluid">
             <div class="row map_addres">
                 <div class="col">
-                    <div class="map_text"><img src="assets/images/map.ico"><span class="map_icon">Profesor José García Santesmases 9, 28040, Madrid.</span></div>
+                    <div class="map_text"><a class="map_img" href="https://www.bing.com/maps?q=ucm+fdi&cvid=2529d3808d0f44b4b07989f7c47bdba5&aqs=edge.0.69i59j0l6j69i60l2.1522j0j1&pglt=299&FORM=ANNTA1&PC=U531"><img src="assets/images/map.ico"></a><a href="https://www.bing.com/maps?q=ucm+fdi&cvid=2529d3808d0f44b4b07989f7c47bdba5&aqs=edge.0.69i59j0l6j69i60l2.1522j0j1&pglt=299&FORM=ANNTA1&PC=U531" class="map_icon">Profesor José García Santesmases 9, 28040, Madrid.</a></div>
                 </div>
                 <div class="col">
-                    <div class="map_text"><img src="assets/images/phone.ico"><span class="map_icon">+34 913 947 597</span></div>
+                    <div class="map_text"><a class="map_img" href="tel:+34913947597"><img src="assets/images/phone.ico"></a><a href="tel:+34913947597" class="map_icon">+34 913 947 597</a></div>
                 </div>
                 <div class="col">
-                    <div class="map_text"><img src="assets/images/mail.ico"><span class="map_icon">aves-grupo12@gmail.com</span></div>
+                    <div class="map_text"><a class="map_img" href="mailto:aves-grupo12@gmail.com"><img src="assets/images/mail.ico"></a><a href="mailto:aves-grupo12@gmail.com" class="map_icon">aves-grupo12@gmail.com</a></div>
                 </div>
             </div>
             <div class="social_icon">
--- assets/web_utils/head-info.js
+++ assets/web_utils/head-info.js
@@ -3,16 +3,13 @@ document.write(`
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- mobile metas -->
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="viewport" content="initial-scale=1, maximum-scale=1">
 <!-- site metas -->
-<title>Fitcento</title>
-<meta name="keywords" content="">
-<meta name="description" content="">
-<meta name="author" content="">
+<title>Aves - Grupo_12</title>
+<meta name="keywords" content="javascript phaser3 pivotal tracker github css html ucm fdi acedpol cubells pablo hotline miami boxhead aves pájaros cuervo">
+<meta name="description" content="página web dedicada al proyecto de pvli de la ucm fdi. web page dedicated for pvli's project from ucm fdi.">
+<meta name="author" content="Acedpol. Pablo Cubells.">
 <!-- fevicon -->
-<link rel="icon" href="images/fb-icon.png" type="image/gif" />
+<link rel="icon" href="./assets/images/logo-cuervo.ico" type="image/x-icon" />
 <!-- game style sheet -->
 <script src="./videojuego/lib/phaser.min.js"></script>
 <link rel="stylesheet" href="./assets/css/aves-style.css">
